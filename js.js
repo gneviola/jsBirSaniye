a496a02962e4e00ae2401eee270bcbd014c29076
vPC <vPC@DESKTOP-4U88UR1>
2019-11-20 15:01:44 +0100

    Geri sayim eklendi

--- js.js
+++ js.js
@@ -11,10 +11,30 @@ async function bekle() {
         sifirinciDugme.textContent = "padding = " + i;
         await sleep(50);
         sifirinciDugme.onclick = null;
+        birinciDugme.onclick = null;
     };
     // onclick null oldugu icin baska yoloni bulamadim
     sifirinciDugme.onclick = bekle
+    birinciDugme.onclick = geri
 };
+
+async function geri() {
+
+    for (let i = 50; i >= 0; i--) {
+        sifirinciDugme.style.padding = i + "px";
+        birinciDugme.style.margin = i + "px";
+        birinciDugme.textContent = "margin = " + i;
+        sifirinciDugme.textContent = "padding = " + i;
+        await sleep(50);
+        sifirinciDugme.onclick = null;
+        birinciDugme.onclick = null;
+    };
+    // onclick null oldugu icin baska yoloni bulamadim
+    sifirinciDugme.onclick = bekle
+    birinciDugme.onclick=geri
+};
+
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
